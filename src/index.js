ce7ae7c15f79832156066db5930c4b1187cb134e
Nick Weiland <njweiland23@gmail.com>
2023-11-02 15:23:17 -0500

    fixed website

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  // document.getElementsByClass()
+  // element.addEventListener("click", )
+
+  // need to record input field when "create new task" is clicked. 
+    // get form
+    
+    const callForm = document.getElementById("create-task-form")
+    const newList = document.getElementById("tasks")
+    // add event listener, submit 
+    callForm.addEventListener("submit", function addItem(e) {
+      e.preventDefault()
+      const input = document.getElementById("new-task-description").value
+      const li = document.createElement("li")
+      li.textContent = input
+      newList.appendChild(li)
+      console.log("Hi")
+  })
+
+    // create new list item in unordered list
 });
